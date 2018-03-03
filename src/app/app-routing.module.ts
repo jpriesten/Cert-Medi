66568925a60681d71884a75f9a529578ddf14241
unknown <jpriesten>
2018-03-03 21:25:53 +0100

    Made the routing easy. It should pretty easy to continue managing the routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutsComponent } from './layouts/layouts.component';
+import { SectionProductListComponent } from './section-product-list/section-product-list.component';
 
 const routes: Routes = [
 
   { path: 'home', component: LayoutsComponent },
+  { path: 'produits', component: SectionProductListComponent },
+  { path: 'home', component: LayoutsComponent },
+  { path: 'home', component: LayoutsComponent},
   { path: '', redirectTo: '/home', pathMatch: 'full'}
 
 ];
