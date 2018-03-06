2364af9420b73da20f13433955e964da6c3b25ca
unknown <jpriesten>
2018-03-06 11:19:51 +0100

    Successfully setup mail system. Once a user sends a contact email, the admin will receive it

--- functions/index.js
+++ functions/index.js
@@ -13,6 +13,31 @@ exports.addMessage = functions.https.onRequest((req, res) => {
     // Push the new message into the Realtime Database using the Firebase Admin SDK.
     return admin.database().ref('/messages').push({original: original}).then((snapshot) => {
       // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-      return res.redirect(303, snapshot.ref);
+      return res.redirect(303, snapshot.ref); 
     });
+  });
+
+// Configuring the nodemailer
+const nodemailer = require('nodemailer');
+const gmailEmail = encodeURIComponent(functions.config().gmail.email);
+const gmailPassword = encodeURIComponent(functions.config().gmail.password);
+const mailTransport = nodemailer.createTransport(`smtps://${gmailEmail}:${gmailPassword}@smtp.gmail.com`);
+
+exports.sendContactMessage = functions.database.ref('/messages/{pushKey}').onWrite(event => {
+    const snapshot = event.data;
+  // Only send email for new messages.
+    if (snapshot.previous.val() || !snapshot.val().name) {
+      return;
+    }
+    
+    const val = snapshot.val();
+    
+    const mailOptions = {
+      to: 'jepriesten@gmail.com',
+      subject: `Information Request from ${val.name}`,
+      html: val.html
+    };
+    return mailTransport.sendMail(mailOptions).then(() => {
+      return console.log('Mail sent to: jepriesten@gmail.com')}
+    );
   });
--- src/app/contact.service.ts
+++ src/app/contact.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Contact } from './contact';
 import { AngularFireModule } from 'angularfire2';
-import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireDatabase } from 'angularfire2/database'; 
 
 @Injectable()
 export class ContactService {
